aaaacdb0fe66a4b83fc4ab9a44073b82671f23b5
Andrii <kulchaka@gmail.com>
2021-09-24 11:00:37 +0200

    add unsuccessful search

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -3,12 +3,17 @@ import Card from "./Card";
 
 const Movies = (props) => {
 
-  const {movies} = props
+  const {movies = []} = props
 
   return (
       <div className="container movies">
         {
-          movies.map(e => <Card key={e.imdbID} {...e} />)
+          movies.length
+              ?
+              (movies.map(e => <Card key={e.imdbID} {...e} />))
+              :
+              (<h4>nothing found...</h4>)
+
         }
       </div>
   );
--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -7,36 +7,37 @@ class Main extends Component {
 
   state = {
     movies: [],
-    errorSearch: true,
-    type: 'series'
+    loading: true
   }
 
   componentDidMount() {
     fetch('http://www.omdbapi.com/?apikey=47b0015f&s=matrix')
         .then(respone => respone.json())
-        .then(data => this.setState({movies: data.Search}))
+        .then(data => this.setState({movies: data.Search, loading: false}))
   }
 
   searchMovies = (str, type) => {
-    console.log('Fecth', type)
+    this.setState({loading: true})
     fetch(`http://www.omdbapi.com/?apikey=47b0015f&s=${str}&type=${type}`)
         .then(respone => respone.json())
-        .then(data => this.setState({movies: data.Search}))
-        .catch(err => alert(err))
+        .then(data => this.setState({movies: data.Search, loading: false}))
   }
 
   render() {
+    const {movies, loading} = this.state
     return (
         <main>
+          <Search search={this.searchMovies}/>
           {
-            this.state.movies.length
+            loading
                 ?
-                (<>
-                  <Search search={this.searchMovies}/>
-                  <Movies movies={this.state.movies}/>
-                </>)
-                :
                 (<Preloader/>)
+
+                :
+                (
+                    <Movies movies={movies}/>
+                )
+
           }
 
         </main>
